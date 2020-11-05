1524fffae1fcf1509479f2c2208fba84d1555ea6
Emmanuel Roux <fesaille@users.noreply.github.com>
2020-11-05 23:33:23 +0100

    Install watermark if not present

--- python/.ipython/profile_default/ipython_config.py
+++ python/.ipython/profile_default/ipython_config.py
@@ -1,2 +1,13 @@
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.editor = 'vim'
+c = get_config()
+
+try:
+    import watermark
+except ModuleNotFoundError:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "watermark"])
+
+c.InteractiveShellApp.extensions = ["watermark"]
+c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.editor = "vim"
